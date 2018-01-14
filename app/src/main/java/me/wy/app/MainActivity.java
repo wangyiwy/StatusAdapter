b76e773a8d34f6174839bf74992da38c77ebe1b4
[wangyi] <[wangyi@prosysoft.com]>
2018-01-14 14:34:27 +0800

    Initial commit

---
+++ app/src/main/java/me/wy/app/MainActivity.java
@@ -0,0 +1,177 @@
+package me.wy.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+
+    private List<String> mDataList;
+    private MyAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mDataList = new ArrayList<>();
+        mAdapter = new MyAdapter(mDataList);
+        //添加点击事件
+        mAdapter.setOnStatusViewClickListener(new StatusAdapter.OnStatusViewClickListener() {
+            @Override
+            public void onEmptyViewClick(View view) {
+                Toast.makeText(view.getContext(), "OnEmptyViewClick",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onErrorViewClick(View view) {
+                Toast.makeText(view.getContext(), "OnErrorViewClick",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_show_loading:
+                mAdapter.showLoading();
+                break;
+            case R.id.menu_show_empty:
+                mAdapter.showEmpty();
+                break;
+            case R.id.menu_show_error:
+                mAdapter.showError();
+                break;
+            case R.id.menu_show_normal:
+                mAdapter.showNormal();
+                break;
+            case R.id.menu_add_data:
+                mDataList.add("");
+                mAdapter.showNormal();
+                break;
+            case R.id.menu_remove_data:
+                if (mDataList.size() > 0) {
+                    mDataList.remove(0);
+                }
+                mAdapter.notifyDataSetChanged();
+                break;
+        }
+        return false;
+    }
+
+    private class MyAdapter extends StatusAdapter<RecyclerView.ViewHolder> {
+        private final int VIEW_TYPE_TEXT = 1;
+        private final int VIEW_TYPE_IMAGE = 2;
+
+        private List<String> mDataList;
+
+        public MyAdapter(List<String> dataList) {
+            this.mDataList = dataList;
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateDataViewHolder(ViewGroup parent, int viewType) {
+            if (viewType == VIEW_TYPE_TEXT) {
+                View itemView = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_list_text, parent, false);
+                return new TextDataHolder(itemView);
+            } else {
+                View itemView = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_list_image, parent, false);
+                return new ImageDataHolder(itemView);
+            }
+        }
+
+        @Override
+        public void onBindDataViewHolder(RecyclerView.ViewHolder holder, int position) {
+            if (holder instanceof TextDataHolder) {
+                TextDataHolder textDataHolder = (TextDataHolder) holder;
+                textDataHolder.textView.setText("Hello World! " + position);
+            } else if (holder instanceof ImageDataHolder) {
+                //todo bindImageDataViewHolder
+            }
+        }
+
+        @Override
+        public int getDataItemCount() {
+            return mDataList.size();
+        }
+
+        @Override
+        public int getDataItemType(int position) {
+            if (position % 2 == 0) {
+                return VIEW_TYPE_TEXT;
+            } else {
+                return VIEW_TYPE_IMAGE;
+            }
+        }
+
+        @Override
+        protected void bindEmptyViewHolder(RecyclerView.ViewHolder holder) {
+            super.bindEmptyViewHolder(holder);
+            //可以在这里实现自定义操作
+        }
+
+        @Override
+        protected void bindErrorViewHolder(RecyclerView.ViewHolder holder) {
+            super.bindErrorViewHolder(holder);
+            //可以在这里实现自定义操作
+        }
+
+        @Override
+        protected int getLoadingLayout() {
+            //重写此方法或者createLoadingViewHolder方法修改LoadingView
+            return super.getLoadingLayout();
+        }
+
+        @Override
+        protected int getEmptyLayout() {
+            //重写此方法或者createEmptyViewHolder方法修改EmptyView
+            return super.getEmptyLayout();
+        }
+
+        @Override
+        protected int getErrorLayout() {
+            //重写此方法或者createErrorViewHolder方法修改ErrorView
+            return super.getErrorLayout();
+        }
+    }
+
+    private class TextDataHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public TextDataHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+
+    private class ImageDataHolder extends RecyclerView.ViewHolder {
+        public ImageDataHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
---
+++ app/src/main/java/me/wy/app/StatusAdapter.java
@@ -0,0 +1,217 @@
+package me.wy.app;
+
+import android.support.annotation.IntDef;
+import android.support.annotation.LayoutRes;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * author: WangYi
+ * created on: 2018/1/11 20:54
+ * description:
+ */
+
+public abstract class StatusAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter {
+    private static final String TAG = "StatusAdapter";
+
+    //加载中
+    public static final int STATUS_LOADING = 100;
+    //空数据
+    public static final int STATUS_EMPTY = 200;
+    //加载错误
+    public static final int STATUS_ERROR = 300;
+    //正常状态
+    public static final int STATUS_NORMAL = 400;
+
+    @IntDef({STATUS_LOADING, STATUS_EMPTY, STATUS_ERROR, STATUS_NORMAL})
+    public @interface AdapterStatus {
+    }
+
+    @AdapterStatus
+    private int mStatus = STATUS_LOADING;
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case STATUS_LOADING:
+                return createLoadingViewHolder(parent);
+            case STATUS_EMPTY:
+                return createEmptyViewHolder(parent);
+            case STATUS_ERROR:
+                return createErrorViewHolder(parent);
+            case STATUS_NORMAL:
+            default:
+                return onCreateDataViewHolder(parent, viewType);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (mStatus == STATUS_NORMAL) {
+            onBindDataViewHolder((VH) holder, position);
+        } else if (mStatus == STATUS_ERROR) {
+            bindErrorViewHolder(holder);
+        } else if (mStatus == STATUS_EMPTY) {
+            bindEmptyViewHolder(holder);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        switch (mStatus) {
+            case STATUS_LOADING:
+            case STATUS_EMPTY:
+            case STATUS_ERROR:
+                return 1;
+            case STATUS_NORMAL:
+            default:
+                return getDataItemCount();
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mStatus == STATUS_NORMAL) {
+            return getDataItemType(position);
+        }
+        return mStatus;
+    }
+
+    public void showLoading() {
+        this.mStatus = STATUS_LOADING;
+        notifyDataSetChanged();
+    }
+
+    public void showEmpty() {
+        this.mStatus = STATUS_EMPTY;
+        notifyDataSetChanged();
+    }
+
+    public void showError() {
+        this.mStatus = STATUS_ERROR;
+        notifyDataSetChanged();
+    }
+
+    public void showNormal() {
+        this.mStatus = STATUS_NORMAL;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        registerAdapterDataObserver(observer);
+    }
+
+    private final RecyclerView.AdapterDataObserver observer =
+            new RecyclerView.AdapterDataObserver() {
+                @Override
+                public void onChanged() {
+                    //自动显示空数据状态
+                    if (getDataItemCount() == 0 && mStatus == STATUS_NORMAL) {
+                        showEmpty();
+                    }
+                }
+
+                @Override
+                public void onItemRangeInserted(int positionStart, int itemCount) {
+                }
+
+                @Override
+                public void onItemRangeRemoved(int positionStart, int itemCount) {
+                }
+            };
+
+    public class StatusViewHolder extends RecyclerView.ViewHolder {
+        public StatusViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    @LayoutRes
+    protected int getLoadingLayout() {
+        return R.layout.layout_loading_view;
+    }
+
+    @LayoutRes
+    protected int getEmptyLayout() {
+        return R.layout.layout_empty_view;
+    }
+
+    @LayoutRes
+    protected int getErrorLayout() {
+        return R.layout.layout_error_view;
+    }
+
+    protected RecyclerView.ViewHolder createLoadingViewHolder(ViewGroup parent) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(getLoadingLayout(), parent, false);
+        Log.d(TAG, "createLoadingViewHolder");
+        return new StatusViewHolder(itemView);
+    }
+
+    protected RecyclerView.ViewHolder createEmptyViewHolder(ViewGroup parent) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(getEmptyLayout(), parent, false);
+        Log.d(TAG, "createEmptyViewHolder");
+        return new StatusViewHolder(itemView);
+    }
+
+    protected RecyclerView.ViewHolder createErrorViewHolder(ViewGroup parent) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(getErrorLayout(), parent, false);
+        Log.d(TAG, "createErrorViewHolder");
+        return new StatusViewHolder(itemView);
+    }
+
+    private OnStatusViewClickListener mOnStatusViewListener;
+
+    public interface OnStatusViewClickListener {
+        void onEmptyViewClick(View view);
+
+        void onErrorViewClick(View view);
+    }
+
+    public void setOnStatusViewClickListener(OnStatusViewClickListener listener) {
+        this.mOnStatusViewListener = listener;
+    }
+
+    public OnStatusViewClickListener getOnStatusViewClickListener() {
+        return mOnStatusViewListener;
+    }
+
+    protected void bindEmptyViewHolder(RecyclerView.ViewHolder holder) {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mOnStatusViewListener != null) {
+                    mOnStatusViewListener.onErrorViewClick(v);
+                }
+            }
+        });
+    }
+
+    protected void bindErrorViewHolder(RecyclerView.ViewHolder holder) {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mOnStatusViewListener != null) {
+                    mOnStatusViewListener.onEmptyViewClick(v);
+                }
+            }
+        });
+    }
+
+    public abstract RecyclerView.ViewHolder onCreateDataViewHolder(ViewGroup parent, int viewType);
+
+    public abstract void onBindDataViewHolder(VH holder, int position);
+
+    public abstract int getDataItemCount();
+
+    public int getDataItemType(int position) {
+        return 0;
+    }
+}
